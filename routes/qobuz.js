02831d03fdefd0eea18d483d2ec4f9b96872e691
ashish <ashishmaurya0831@gmail.com>
2025-11-15 18:49:49 +0530

    initial commit

---
+++ routes/qobuz.js
@@ -0,0 +1,38 @@
+
+import dotenv from 'dotenv';
+import Qobuz from 'lucida/streamers/qobuz';
+import express from 'express';
+
+const route = express.Router();
+
+dotenv.config();
+
+const client = new Qobuz({
+  token: process.env.QOBUZ_USER_TOKEN,
+  appSecret: process.env.QOBUZ_APP_SECRET,
+  appId: process.env.QOBUZ_APP_ID,
+})
+
+
+route.get('/test', (req, res) => {
+  res.send('Qobuz route is working!');
+});
+
+route.get('/search', async (req, res) => {
+  const { query } = req.query;
+  const result = await client.search(query);
+  res.send(result.tracks);
+}); 
+
+
+route.get('/stream', async (req, res) => {
+  const { url } = req.query;
+  const result = await client.getByUrl(url);
+  const stream = await result.getStream();
+  res.setHeader('Content-Type', stream.mimeType);
+  res.setHeader('Content-Length', stream.sizeBytes);
+  res.setHeader("Accept-Ranges", "bytes");
+  stream.stream.pipe(res);
+});
+
+export default route;
---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import route from './routes/qobuz.js';
+const app = express();
+const port = 3000;
+
+
+app.use('/qobuz', route);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
